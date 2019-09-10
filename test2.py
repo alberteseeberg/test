8544a0300ced45df3f4384f5d26e05be0037e329
alberteseeberg <alberteseeberg@hotmail.com>
2019-09-10 12:48:36 +0200

    yaaaay

--- test2.py
+++ test2.py
@@ -6,3 +6,50 @@ print("hello world")
 
 43+5
 print("hellooooo whaaaale")
+d=2+4
+
+
+"a string"*3
+
+print(f"{d} which is a number of type {type(d)}")
+
+string = "this is a string"
+
+a_list_of_strings=string.split(" ")
+a_list_of_strings(0)
+
+def print_hello():
+    print("hello world")
+
+new_name = print_hello
+
+
+def add_hello_to_string(string):
+    return string +"hello"
+
+    add_hello_to_string("test")
+
+
+#create a string regarding westworld which uses: addition
+
+"westworld" + " " + "rocks"
+
+#create a string regarding westworld which uses: multiplication
+
+"westworld" + " " + "rocks" * 10
+
+#create a string regarding westworld which uses: At least one object method
+
+westworld = "westworld"
+
+westworld.capitalize()
+
+"hej".capitalize()
+
+
+def calculator(x,y):
+    answer = x + y
+    print(f"{x}+{y}={answer}")
+
+
+calculator(2,5)
